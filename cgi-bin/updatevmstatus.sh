d21d05eda6a4c13072cae70eb3842118a822b658
Khalid Ahmed <khahmed@rim.com>
2012-09-23 17:30:11 -0400

    Added copyright headers

--- cgi-bin/updatevmstatus.sh
+++ cgi-bin/updatevmstatus.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-# This updates the IP address of the VM  assigned by dyamic DNS. This script
-# will be invoked from CGI on the ONE master server and will be triggered via 
-# wget from the contextualization script
+# -------------------------------------------------------------------------- #
+# Copyright 2011-2012, Research In Motion (rim.com)                          #
+#                                                                            #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
+# not use this file except in compliance with the License. You may obtain    #
+# a copy of the License at                                                   #
+#                                                                            #
+# http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                            #
+# Unless required by applicable law or agreed to in writing, software        #
+# distributed under the License is distributed on an "AS IS" BASIS,          #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
+# See the License for the specific language governing permissions and        #
+# limitations under the License.                                             #
+#--------------------------------------------------------------------------- #
 LOGFILE=/tmp/updatevmstatus.log
 echo "Content-type: text/html"
 echo ""
